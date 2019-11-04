91e61cb67146687fed15d6a3ebac1bfdfe0a3213
Damola Salisu <salisu.damola@yahoo.com>
2019-11-04 12:57:11 +0100

    initial files

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,41 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 // Components
-import Layout from '../../container/DashboardWrapper'
+import Layout from "../../container/DashboardWrapper";
 
 // auth
 
 // import temporarily
-import Dashboard from './oldIndex'
+import Dashboard from "./oldIndex";
 
 // Generic Components
-import { Wrapper, Row } from '../../theme/style/styles'
+import { Wrapper, Row } from "../../theme/style/styles";
 const styles = {
   Wrapper: {
-    height: 'calc(94vh - 103px)'
+    height: "calc(94vh - 103px)"
   },
   Row: {
-    padding: '0 0 0 calc(120px)'
+    padding: "0 0 0 200px"
   }
-}
+};
 
 class CompanyDashboard extends Component {
-  render () {
+  render() {
     return (
       <Layout>
-        <Wrapper
-          className='fixedheight'
-          {...styles.Wrapper}>
-          <Row
-
-            {...styles.Row}>
-            {/* <Dashboard /> */}
-          </Row>
-
+        <Wrapper className="fixedheight" {...styles.Wrapper}>
+          <Row {...styles.Row}>{/* <Dashboard /> */}</Row>
         </Wrapper>
       </Layout>
-    )
+    );
   }
 }
 
-export default CompanyDashboard
+export default CompanyDashboard;
